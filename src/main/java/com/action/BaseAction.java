059e75768d8cb3750c1b3a9d1e6e06f01103324a
lvfajin <lvfajin@163.com>
2014-04-21 17:22:54 +0800

    finish fix and merge dev

--- src/main/java/com/action/BaseAction.java
+++ src/main/java/com/action/BaseAction.java
@@ -1,10 +1,7 @@
 package com.action;
 
-<<<<<<< HEAD
-=======
 import com.dao.BaseDao;
 
->>>>>>> 4f662c61d5efd9257f3ca630db0b4e954fafd415
 public class BaseAction {
 	//初始化Dao层
 	protected BaseDao dao=null;

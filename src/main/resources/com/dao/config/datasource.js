be4cfad41f71161fbbde7f75fdacf219e8d66393
HeJian <hellojian123@gmail.com>
2014-04-27 13:39:50 +0800

    Modify datasource file Configuration postgresql conect info

--- src/main/resources/com/dao/config/datasource.js
+++ src/main/resources/com/dao/config/datasource.js
@@ -6,9 +6,9 @@ datasource:{
 		},
 		fields:{
 			driverClassName:"com.mysql.jdbc.Driver",
-			url:"jdbc:mysql://127.0.0.1:3306/maven_test?useUnicode=true&characterEncoding=utf-8",
-			username:"root",
-			password:"root"
+			url:"jdbc:postgresql://ec2-107-22-190-179.compute-1.amazonaws.com:5432/db24nor561gp9p",
+			username:"loyaxjftushvqg",
+			password:"Kc8o-RjfUF9Mww4kAMfQZsum8M"
 		}
 	},
 	dao : {

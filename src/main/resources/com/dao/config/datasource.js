d07c3322369c718c4d1091304cfde25a05b0c383
HeJian <hellojian123@gmail.com>
2014-04-28 11:49:53 +0800

    add log file

--- src/main/resources/com/dao/config/datasource.js
+++ src/main/resources/com/dao/config/datasource.js
@@ -11,12 +11,12 @@ var ioc = {
 			username:"loyaxjftushvqg",
 			password:"Kc8o-RjfUF9Mww4kAMfQZsum8M"
 
-/*
+			/*
 			driverClassName:"com.mysql.jdbc.Driver",
 			url:"jdbc:mysql://127.0.0.1:3306/maven_test?useUnicode=true&characterEncoding=utf-8",
 			username:"root",
 			password:"root"
-*/
+			 */
 		}
 	},
 	dao : {
